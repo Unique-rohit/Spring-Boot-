aa9b9222e680e775d7d884a65bd9388224d7e68e
Unique-rohit <94671201+Unique-rohit@users.noreply.github.com>
2022-11-25 15:13:19 +0530

    Add files via upload

---
+++ spring-elastic-demo/src/test/java/com/techprimers/elastic/springelasticdemo/SpringElasticDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techprimers.elastic.springelasticdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringElasticDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
